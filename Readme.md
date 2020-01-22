ebfde7f7d491369052de2fa4a3a8fd505dc82d9a
Mann <pareshmaniyar1@gmail.com>
2020-01-22 21:42:04 +0530

    Add part 2 and part 3 solution

---
+++ Readme.md
@@ -0,0 +1,20 @@
+Problem statement: A school has created its timetable based on availability of six subject teachers (see Teacher wise class timetable PDF file). 
+Using this as source, also available as csv files per teacher, answer the following questions:
+
+1. Read the CSV files and generate class wise timetable (Write a simple csv parser)
+// Done
+2. Assuming that any teacher can be co-teacher to any other class, generate a new timetable such that no teacher is idle during the duration of school.
+// Alloting idle classes
+1. Allot for one teacher until she is filled
+2. To improve create a roundrobin for allotment
+
+3. Identify when all teachers are busy and a class can not be assigned a co-teacher. Calculate minimum number of extra co-teachers needed to solve it.
+//Keep assigning the idle classes to teacher until she is idle
+
+Important points:
+----------------
+1. You need to commit the code to a GitHub repository and send us the link to it.
+2. You can create the UI to your preferences as your backend code writing is more important.
+3. You should solve the above problems yourself, i.e., you can't use plugins or libraries to solve the above problem. You can however use any sort of libraries on the front end.
+4. You should provide clear instructions on how to run your code 
+
--- app.js
+++ app.js
@@ -6,22 +6,35 @@ var logger = require('morgan');
 const csv = require('csv-parser');
 const fs = require('fs');
 let timetable = {};
+let teacherSchedule = {};
 let english = [];
-let classes = ["10th","9th","8th","7th","6th"];
+//Can generaize below code
+let classes = ["10th","9th","8th","7th","6th", "5th"];
 let days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 let files = ["English", "Hindi", "Kannada", "Maths", "Science"];
-for(let i = 0; i < 5; i++){
+let teachers = ["Teacher 1", "Teacher 2", "Teacher 3", "Teacher 4", "Teacher 5", "Teacher 6"];
+
+for(let i = 0; i < classes.length; i++){
   timetable[classes[i]] = {};
-  for(let j = 0;j < 6;j++){
+  for(let j = 0;j < days.length;j++){
     timetable[classes[i]][days[j]] = [];
     for(let k = 0; k < 9; k++){
       timetable[classes[i]][days[j]].push(null);
     }
   }
 }
+for(let i = 0; i < files.length; i++){
+  teacherSchedule[files[i]] = {};
+  for(let j = 0;j < days.length;j++){
+    teacherSchedule[files[i]][days[j]] = [];
+    for(let k = 0; k < 9; k++){
+      teacherSchedule[files[i]][days[j]].push(null);
+    }
+  }
+}
 console.log(timetable);
-files.forEach((val) => {
-fs.createReadStream(val + '.csv')
+files.forEach((Subject) => {
+fs.createReadStream(Subject + '.csv')
   .pipe(csv())
   .on('data', (row) => {
     let classTime = 0;
@@ -35,20 +48,67 @@ fs.createReadStream(val + '.csv')
     }
     for(let [key, value] of Object.entries(row)) {
       if(value != '' && key != "--"){
-        // try {
-          timetable[value.trim()][key][classTime] = val;
-        // } catch(e){
-        //   console.log("Error: value "+ value + ", key: " + key + ", classTime: " + classTime + ", Sub: " + val);
-        //   console.log(timetable[value]);
-        // }
+        try {
+          timetable[value.trim()][key][classTime] = Subject;
+          teacherSchedule[Subject][key][classTime] = value.trim();
+        } catch(e){
+          console.log("Error: value "+ value + ", key: " + key + ", classTime: " + classTime + ", Sub: " + Subject);
+        }
       }
     }
     english.push(row);
     console.log(row);
   })
   .on('end', () => {
-    // console.log(english);
+    console.log(english);
     console.log(timetable);
+    //Assigning Teachers each Subject
+    let counter = 1;
+    console.log("Entered/////////////////////////////////////////////////////");
+    for(let eachClass of Object.keys(timetable)) {
+      for(let day of Object.keys(timetable[eachClass])) {
+        console.log("day ",day);
+        timetable[eachClass][day].forEach((timeSlot, index) => {
+          // console.log("eachClass "+ eachClass + ", day: " + day + ", index: " + index + ", timeSlot: " + timeSlot + ", val: " + timetable[eachClass][day]);
+          if(!timeSlot){
+            console.log("Null *********************&&&&&&&&&&&&&&&&&&&&&&&&&&&***************");
+            // console.log("otherClass "+ otherClass + ", day: " + day + ", index: " + index + ", timeSlot: " + timeSlot + ", val: " + timetable[eachClass][day]);
+            //check for availability of other teachers
+            let newTeachersList = {};
+            files.forEach((teacher) => {
+              newTeachersList[teacher] = true;
+            });
+            for(let otherClass of Object.keys(timetable)) {
+              console.log("otherClass "+ otherClass + ", day: " + day + ", index: " + index + ", timeSlot: " + timeSlot + ", val: " + timetable[eachClass][day]);
+              if(newTeachersList.hasOwnProperty(timetable[otherClass][day][index])){
+                delete newTeachersList[timetable[otherClass][day][index]];
+              }
+            }
+            if(Object.keys(newTeachersList).length === 0 && newTeachersList.constructor === Object){
+              console.log("/////////////////////////////////////////////////////////////////");
+              timetable[eachClass][day][index] = ("Teacher " + counter);
+              files.push("Teacher " + counter);
+              counter = counter + 1;
+            } else {
+              timetable[eachClass][day][index] = Object.keys(newTeachersList)[0];
+              console.log("Object.keys(newTeachersList)[0]: ", Object.keys(newTeachersList)[0], ", day:", day, ", index:", index, ", eachClass: ", eachClass);
+              if(!teacherSchedule.hasOwnProperty(Object.keys(newTeachersList)[0])){
+                console.log("!!!!!!!!!!!!!!inside");
+                teacherSchedule[Object.keys(newTeachersList)[0]] = {};
+                for(let j = 0;j < 6;j++){
+                  teacherSchedule[Object.keys(newTeachersList)[0]][days[j]] = [];
+                  for(let k = 0; k < 9; k++){
+                    teacherSchedule[Object.keys(newTeachersList)[0]][days[j]].push(null);
+                  }
+                }
+              }
+              // console.log(teacherSchedule);
+              teacherSchedule[Object.keys(newTeachersList)[0]][day][index] = eachClass + "Updated";
+            }
+          }
+        })
+      }
+    }
     console.log('CSV file successfully processed');
   });
 });
@@ -69,7 +129,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){ 
-  res.render('index',{title:"Express", timetable}) 
+  res.render('index',{title:"Express", timetable, teacherSchedule}) 
 }); 
 // app.use('/', indexRouter, timetable);
 app.use('/users', usersRouter);
